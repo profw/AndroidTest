08238682e14f4ced1e789b71dea31548ec20a69a
Alex Sh <alexrims@gmail.com>
2025-03-04 23:37:17 +0300

    Add land orientation and prev button

--- app/src/main/java/com/example/geoquizx/MainActivity.kt
+++ app/src/main/java/com/example/geoquizx/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.geoquizx
 
 import android.os.Bundle
-import android.view.Gravity
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.geoquizx.model.Question
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var prevButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
@@ -23,17 +28,21 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_mideast, false),
         Question(R.string.question_africa, false),
         Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
+        Question(R.string.question_asia, true)
+    )
 
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        prevButton = findViewById(R.id.prev_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
 
@@ -46,27 +55,71 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            currentIndex++
+            if (currentIndex >= questionBank.size) {
+                currentIndex = questionBank.size - 1
+            }
+
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener {
+            currentIndex--
+            if (currentIndex < 0) {
+                currentIndex = 0
+            }
+
             updateQuestion()
         }
 
         updateQuestion()
-        /*
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-         */
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
+        if (currentIndex < 0) {
+            return
+        }
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+        prevButton.isEnabled = currentIndex > 0
+        nextButton.isEnabled = currentIndex < questionBank.size - 1
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
+        if (currentIndex < 0) {
+            return
+        }
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
